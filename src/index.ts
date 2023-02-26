c2e5a967c2fe42ab7821a181149adcbff35edc2c
Alejandro2097 <alejo12381@hotmail.es>
2023-02-25 23:07:58 -0500

    observers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { Observable } from 'rxjs';
+import { Observer } from 'rxjs/internal/types';
 
+const observer: Observer<any> = {
+    next: value => console.log('Siguiente [next]:', value),
+    error: error => console.warn('error [obs]:', error),
+    complete: () => console.info('completado [obs]')
+}
 // const obs$ = Observable.create();
 
 const obs$ = new Observable<string>( subs => {
@@ -9,7 +15,17 @@ const obs$ = new Observable<string>( subs => {
     subs.next('Hola');
     subs.next('mundo');
 
+    // Forzar un error 
+    const a = undefined;
+    a.nombre = 'Fernando';
+
     subs.complete();
 });
 
-obs$.subscribe(console.log);
+obs$.subscribe(observer);
+
+// obs$.subscribe(
+//     valor => console.log('next', valor),
+//     error => console.warn('error', error),
+//     () => console.info('Completado')
+// );
