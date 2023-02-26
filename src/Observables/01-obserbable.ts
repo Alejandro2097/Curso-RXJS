6a68c4aa84c9265084eb6578bd2002eeefe7ba34
Alejandro2097 <alejo12381@hotmail.es>
2023-02-25 23:10:43 -0500

    preparing the next class

--- src/Observables/01-obserbable.ts
+++ src/Observables/01-obserbable.ts
@@ -6,26 +6,3 @@ const observer: Observer<any> = {
     error: error => console.warn('error [obs]:', error),
     complete: () => console.info('completado [obs]')
 }
-// const obs$ = Observable.create();
-
-const obs$ = new Observable<string>( subs => {
-    subs.next('Hola');
-    subs.next('mundo');
-
-    subs.next('Hola');
-    subs.next('mundo');
-
-    // Forzar un error 
-    const a = undefined;
-    a.nombre = 'Fernando';
-
-    subs.complete();
-});
-
-obs$.subscribe(observer);
-
-// obs$.subscribe(
-//     valor => console.log('next', valor),
-//     error => console.warn('error', error),
-//     () => console.info('Completado')
-// );
