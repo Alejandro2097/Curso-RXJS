009bd4d96cd408ef575afe97352a0229cdf83df9
Alejandro2097 <alejo12381@hotmail.es>
2023-02-26 18:58:47 -0500

    timeout

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { Observer } from 'rxjs/internal/types';
 
 const observer:Observer<any> ={
@@ -10,35 +10,21 @@ const observer:Observer<any> ={
  
 // const obs$ = Observable.create();
 
+const intervalo$ = new Observable<number>(subs => {
+    const intervalID = setInterval(
+        () =>  subs.next(Math.random()), 1000
+    );
+    return () => clearInterval(intervalID);
+});
+//     SUBJECTS
+//  1 - Casteo multiple
+//  2 - Tambien es un observer
+//  3- Tambien se puede manejar el next error y complete
 
-const intervalos$ = new Observable<number>(subscriber => {
-    // Crear un contador, 1,2,3,4,5
-    let count = 0
-    const interval = setInterval(() =>{
-        // cada segundo
-        count ++;
-        subscriber.next(count);
-        console.log(count);
-    }, 1000);
-    return () => {
-        clearInterval(interval);
-        console.log('Intervalo destruido');
-    }
-})
-const subs1 = intervalos$.subscribe(observer);
-const subs2 = intervalos$.subscribe(observer);
-const subs3 = intervalos$.subscribe(observer);
+const subject$ = new Subject();
+intervalo$.subscribe(subject$);
+// const subs1 = intervalo$.subscribe(rnd => console.log('subs1', rnd));
+// const subs2 = intervalo$.subscribe(rnd => console.log('subs2', rnd));
 
-
-subs1.add();
-subs2.add();
-subs3.add(); 
- 
- 
-setTimeout(() => {
-    
-    subs1.unsubscribe();
-   /* subs2.unsubscribe();
-    subs3.unsubscribe();*/
-    console.log('Completado Timeot');
-}, 6000);
+const subs1 = subject$.subscribe( rnd => console.log('subs1', rnd))
+const subs2 = subject$.subscribe( rnd => console.log('subs2', rnd))
