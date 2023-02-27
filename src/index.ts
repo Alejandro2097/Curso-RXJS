dd37afcf68f458271839332d8e65c3ecbe3452fa
Alejandro2097 <alejo12381@hotmail.es>
2023-02-26 19:23:26 -0500

    subjects

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,10 @@ const intervalo$ = new Observable<number>(subs => {
     const intervalID = setInterval(
         () =>  subs.next(Math.random()), 1000
     );
-    return () => clearInterval(intervalID);
+    return () => {
+        clearInterval(intervalID);
+        console.log('Intervalo destruido');
+    }
 });
 //     SUBJECTS
 //  1 - Casteo multiple
@@ -22,9 +25,15 @@ const intervalo$ = new Observable<number>(subs => {
 //  3- Tambien se puede manejar el next error y complete
 
 const subject$ = new Subject();
-intervalo$.subscribe(subject$);
+const subscription = intervalo$.subscribe(subject$);
 // const subs1 = intervalo$.subscribe(rnd => console.log('subs1', rnd));
 // const subs2 = intervalo$.subscribe(rnd => console.log('subs2', rnd));
 
-const subs1 = subject$.subscribe( rnd => console.log('subs1', rnd))
-const subs2 = subject$.subscribe( rnd => console.log('subs2', rnd))
+const subs1 = subject$.subscribe( observer);
+const subs2 = subject$.subscribe( observer);
+
+setTimeout(() => {
+    subject$.next(10);
+    subject$.complete();
+    subscription.unsubscribe();
+}, 3500);
