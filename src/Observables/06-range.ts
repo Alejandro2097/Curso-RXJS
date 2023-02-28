7aed1678534ee00d2a146a58d1364efaae44f099
Alejandro2097 <alejo12381@hotmail.es>
2023-02-27 22:28:01 -0500

    just another change

--- src/Observables/06-range.ts
+++ src/Observables/06-range.ts
@@ -1,9 +1,9 @@
-import { asyncScheduler, of, range } from 'rxjs';
+import { asyncScheduler, observeOn, of, range } from 'rxjs';
 
 // const src$ = of(1,2,3,4,5);
-const src$ = range(1,5 asyncScheduler);
+const src1$ = range(-5,10).pipe(observeOn(asyncScheduler))
 
 
 console.log('inicio');
-src$.subscribe(console.log);
+src1$.subscribe(console.log);
 console.log('Fin');
