34decf36e18662c1d564c6c3574e6373c23df167
Alejandro2097 <alejo12381@hotmail.es>
2023-03-01 22:22:14 -0500

    filter excercise

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { from, range } from 'rxjs';
+import { filter } from 'rxjs/operators'
+
+range(1,10).pipe(
+    filter((val, i) => {
+        console.log('index', i)
+        return val % 2 === 1
+    })).subscribe(console.log)
+
+const personajes = [
+    {
+        tipo: 'Heroe',
+        nombre: 'Batman'
+    },
+    {
+        tipo: 'Heroe',
+        nombre: 'Robin'
+    },
+    {
+        tipo: 'Villano',
+        nombre: 'Joker'
+    },
+]
+from(personajes).pipe(
+    filter(val =>{
+       return val.tipo === 'Heroe'
+    })
+).subscribe(console.log)
