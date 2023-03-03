6a6ed63fa3fd5209284b0c25143887e0c755ca6e
Alejandro2097 <alejo12381@hotmail.es>
2023-03-02 21:38:32 -0500

    despues

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-import { range, tap } from 'rxjs';
+import { map, range, tap } from 'rxjs';
 
 const numeros$ = range(1,5);
 
 numeros$.pipe(
-    tap( x => console.log('antes', x))
+    tap( x => console.log('antes', x)),
+    map( val => val * 10),
+    tap({
+        next: valor => console.log('despues', valor),
+        complete: () => console.log('Se termino')
+    })
+    
 ).subscribe( val => console.log('subs', val));
