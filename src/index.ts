e61314611e9ab52c57d9fd729063eba0d4942415
Alejandro2097 <alejo12381@hotmail.es>
2023-03-04 23:36:53 -0500

    clean up

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import { fromEvent, take } from 'rxjs';
+import { fromEvent } from 'rxjs';
+import { first, map, take, tap } from 'rxjs/operators'
 
 const click$ = fromEvent<MouseEvent>(document, 'click');
 
 
 click$.pipe(
-    take(1)
-)
+    tap(console.log),
+    // map(event => ({
+    //     clientY: event.clientY,
+    //     clientX: event.clientX
+    // }))
+    map(({ clientY, clientX }) => ({ clientY, clientX })),
+    first(event => event.clientY >= 150)
+) 
 .subscribe({
     next: val => console.log('next: ', val ),
     complete: () => console.log('complete ')
