ad096b9e88889c736730475d9097701ddfa5808c
Alejandro2097 <alejo12381@hotmail.es>
2023-03-06 22:03:43 -0500

    interval

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-
+import { fromEvent, interval } from 'rxjs';
 
 const boton  = document.createElement('button');
 
 boton.innerHTML = 'Detener timer';
 
 document.querySelector('body').append(boton);
+
+const counter$ = interval(1000);
+
+const clickBtn$ = fromEvent(boton, 'click');
+
+counter$
+.subscribe({
+    next: val => console.log('next', val),
+    complete: () => console.log('complete')
+})
