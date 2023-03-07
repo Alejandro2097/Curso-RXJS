d949b8f7c0948dd56c0309b251a066dda55340b6
Alejandro2097 <alejo12381@hotmail.es>
2023-03-06 22:05:10 -0500

    takeUntil

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { fromEvent, interval } from 'rxjs';
+import { fromEvent, interval, takeUntil } from 'rxjs';
 
 const boton  = document.createElement('button');
 
@@ -10,7 +10,9 @@ const counter$ = interval(1000);
 
 const clickBtn$ = fromEvent(boton, 'click');
 
-counter$
+counter$.pipe(
+    takeUntil(clickBtn$)
+)
 .subscribe({
     next: val => console.log('next', val),
     complete: () => console.log('complete')
