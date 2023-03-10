9d800cb44093d35aa50d8631b0bac176a8cd0f46
Alejandro2097 <alejo12381@hotmail.es>
2023-03-09 19:33:06 -0500

    short explanantion

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ const url = 'https://api.github.com/users?per_page=5';
 
 const fetchPromesa = fetch(url);
 
+// fetchPromesa
+//     .then(resp => resp.json())
+//     .then(data => console.log('data: ', data))
+//     .catch(err => console.warn('error en usuarios ', err))
+    
 fetchPromesa
     .then(resp => resp.json())
     .then(data => console.log('data: ', data))
