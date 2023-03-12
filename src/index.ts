10099088e6fe85624563ce2f9d1f6bca7961bc3f
Alejandro2097 <alejo12381@hotmail.es>
2023-03-11 22:16:36 -0500

    url

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-import { ajax } from 'rxjs/ajax';
+import { of } from 'rxjs';
+import { ajax, AjaxError } from 'rxjs/ajax';
+import { catchError } from 'rxjs/operators';
 
 const url = 'https://httpbin.org/delay/1';
 // const url = 'https://api.github.com/users?per_page=5';
 
+const manejaError = (resp: AjaxError) => {
+    console.warn('error', resp.message);
+    return of({
+        ok: false,
+        usuarios: []
+    })
+}
+
+// const obs$ = ajax.getJSON( url).pipe(
+//     catchError(manejaError)
+// );
+// const obs2$ = ajax( url).pipe(
+//     catchError(manejaError)
+// );
+
 const obs$ = ajax.getJSON( url);
 const obs2$ = ajax( url);
 
-obs$.subscribe(data => console.log('data: ', data));
-obs2$.subscribe(data => console.log('data: ', data));
+// obs$.subscribe(data => console.log('data: ', data));
+obs2$.subscribe();
 
