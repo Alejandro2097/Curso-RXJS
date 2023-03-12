78f26776b94a1b988c52a251a5166432620080fd
Alejandro2097 <alejo12381@hotmail.es>
2023-03-11 21:58:44 -0500

    here we go again

--- src/ajax/02-getJson.ts
+++ src/ajax/02-getJson.ts
@@ -1,6 +1,5 @@
 import { ajax } from 'rxjs/ajax';
 
-
 const url = 'https://httpbin.org/delay/1';
 // const url = 'https://api.github.com/users?per_page=5';
 
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,8 @@ import { ajax } from 'rxjs/ajax';
 const url = 'https://httpbin.org/delay/1';
 // const url = 'https://api.github.com/users?per_page=5';
 
-const obs$ = ajax.getJSON( url,  {
-    'Content-Type': 'application/json',
-    'mi-token': 'ABC123'
-} )
+const obs$ = ajax.getJSON( url);
+const obs2$ = ajax( url);
 
 obs$.subscribe(data => console.log('data: ', data));
 
