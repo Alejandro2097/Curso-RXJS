3d79a312f4358602d8486d31ddf496611614f02f
Alejandro2097 <alejo12381@hotmail.es>
2023-03-11 22:00:43 -0500

    flames

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { ajax } from 'rxjs/ajax';
 
-
 const url = 'https://httpbin.org/delay/1';
 // const url = 'https://api.github.com/users?per_page=5';
 
@@ -8,4 +7,5 @@ const obs$ = ajax.getJSON( url);
 const obs2$ = ajax( url);
 
 obs$.subscribe(data => console.log('data: ', data));
+obs2$.subscribe(data => console.log('data: ', data));
 
